0b2834004f947887442834a8595366c47a7724e3
Ace <qanhltv11@gmail.com>
2023-10-19 03:08:18 +0700

    Add project files.

---
+++ frmMyStore.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace DemoDisconnectADO.NET
+{
+    partial class frmMyStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnViewProducts = new Button();
+            btnViewCategories = new Button();
+            btnClose = new Button();
+            dgvData = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // btnViewProducts
+            // 
+            btnViewProducts.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnViewProducts.Location = new Point(86, 69);
+            btnViewProducts.Name = "btnViewProducts";
+            btnViewProducts.Size = new Size(179, 34);
+            btnViewProducts.TabIndex = 0;
+            btnViewProducts.Text = "View Products";
+            btnViewProducts.UseVisualStyleBackColor = true;
+            btnViewProducts.Click += btnViewProducts_Click;
+            // 
+            // btnViewCategories
+            // 
+            btnViewCategories.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnViewCategories.Location = new Point(436, 69);
+            btnViewCategories.Name = "btnViewCategories";
+            btnViewCategories.Size = new Size(169, 34);
+            btnViewCategories.TabIndex = 1;
+            btnViewCategories.Text = "View Categories";
+            btnViewCategories.UseVisualStyleBackColor = true;
+            btnViewCategories.Click += btnViewCategories_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnClose.Location = new Point(296, 403);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(100, 35);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            // 
+            // dgvData
+            // 
+            dgvData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(21, 139);
+            dgvData.Name = "dgvData";
+            dgvData.RowHeadersWidth = 51;
+            dgvData.RowTemplate.Height = 29;
+            dgvData.Size = new Size(654, 258);
+            dgvData.TabIndex = 3;
+            // 
+            // frmMyStore
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(687, 450);
+            Controls.Add(dgvData);
+            Controls.Add(btnClose);
+            Controls.Add(btnViewCategories);
+            Controls.Add(btnViewProducts);
+            Name = "frmMyStore";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmMyStore";
+            Load += frmMyStore_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnViewProducts;
+        private Button btnViewCategories;
+        private Button btnClose;
+        private DataGridView dgvData;
+    }
+}
---
+++ frmMyStore.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoDisconnectADO.NET
+{
+    public partial class frmMyStore : Form
+    {
+        public frmMyStore()
+        {
+            InitializeComponent();
+        }
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var strConnection = config["ConnectionStrings:MyStoreDB"];
+            return strConnection;
+        }
+
+        DataSet dsMyStore = new DataSet();
+
+        private void frmMyStore_Load(object sender, EventArgs e)
+        {
+            DbProviderFactory factory = SqlClientFactory.Instance;
+            using DbConnection conn = factory.CreateConnection();
+            if (conn != null)
+            {
+                Console.WriteLine($"Unable to create the connection object");
+                return;
+            }
+
+            conn.ConnectionString = GetConnectionString();
+            conn.Open();
+
+            string sql = "Select ProductID, ProductName, CategoryID, Price from Products; Select * from Categories";
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, GetConnectionString());
+                adapter.Fill(dsMyStore);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Get Data from Database");
+            }
+        }
+
+        private void btnViewProducts_Click(object sender, EventArgs e)
+        {
+            dgvData.DataSource = dsMyStore.Tables[0];
+        }
+
+        private void btnViewCategories_Click(object sender, EventArgs e)
+        {
+            dgvData.DataSource = dsMyStore.Tables[1];
+        }
+    }
+}
